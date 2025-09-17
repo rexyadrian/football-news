7649277d3b44e5a0f359a6cbf006ed29bab325f9
rexya <rexy.adrian.f@gmail.com>
2025-09-17 14:03:47 +0700

    Complete Tutorial 3: Authentication, Session, Cookies and Selenium

--- main/urls.py
+++ main/urls.py
@@ -7,12 +7,18 @@ from main.views import (
     show_json,
     show_xml_by_id,
     show_json_by_id,
+    register,
+    login_user,
+    logout_user,
 )
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
     path('create-news/', create_news, name='create_news'),
     path('news/<str:id>/', show_news, name='show_news'),
     path('xml/', show_xml, name='show_xml'),
